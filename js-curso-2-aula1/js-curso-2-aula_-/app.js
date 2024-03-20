dbf9ab3369d3de5eb421778c6a92708e4a693a06
Yann Peruzzo <yannperuzzo@gmail.com>
2024-03-20 11:13:34 -0300

    alteração de exibição de texto na tela

--- js-curso-2-aula1/js-curso-2-aula_-/app.js
+++ js-curso-2-aula1/js-curso-2-aula_-/app.js
@@ -9,7 +9,7 @@ function exibirTextoNaTela(tag, texto){
 }
 function exibirMensagemInicial() {
     exibirTextoNaTela('h1', 'Jogo do Número Secreto');
-exibirTextoNaTela('p', 'Escolha um Número entre 1 e 10');
+exibirTextoNaTela('p', 'Escolha um Número entre 1 e 50');
 }
  exibirMensagemInicial();
 
